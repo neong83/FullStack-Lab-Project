1a87c26efe23c86e75b11c7e20156db7cb441152
Ben Ng <neong83@hotmail.com>
2021-10-17 01:44:16 -0700

    connecting all widget to allow user to load all fruits at start and filter fruit upon search

--- frontend/src/App.tsx
+++ frontend/src/App.tsx
@@ -1,24 +1,44 @@
-import React from 'react';
-import logo from './logo.svg';
-import './App.css';
+import "./App.css";
+import React, { useState, useEffect } from "react";
+import { Scroll } from "./components/scroll";
+import { CardList } from "./components/cardList";
+import { getAllFruits, getFruitsByName } from "./connector/fruits";
+import { Search } from "./components/search";
 
 function App() {
+  const [fruits, setFruits] = useState([]);
+  const [searchField, setSearchField] = useState("");
+
+  useEffect(() => {
+    if (searchField === "") {
+      getAllFruits(setFruits);
+    }
+  }, [searchField]);
+
+  useEffect(() => {
+    console.log(`searchField: ${searchField}`);
+  }, [searchField]);
+
+  function searchFruits() {
+    console.log("click search fruit");
+    getFruitsByName(searchField, setFruits);
+  }
+
+  function onSearchChange(event: React.FormEvent<HTMLInputElement>) {
+    setSearchField(event.currentTarget.value);
+  }
+
   return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.tsx</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
+    <div className="tc">
+      <h1 className="f1">Fruit Catalog</h1>
+      <Search onSearchChange={onSearchChange} searchFruits={searchFruits} />
+      <Scroll>
+        {fruits.length == 0 ? (
+          <h2>No fruit name matches with your search, try again</h2>
+        ) : (
+          <CardList data={fruits} />
+        )}
+      </Scroll>
     </div>
   );
 }
