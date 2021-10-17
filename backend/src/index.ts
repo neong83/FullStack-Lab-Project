ecbc78d348b1153a348dff84fe3c91f9415c5a66
Ben Ng <neong83@hotmail.com>
2021-10-17 01:36:38 -0700

    enable CORS for frontend to access content

--- backend/src/index.ts
+++ backend/src/index.ts
@@ -5,6 +5,17 @@ import { router as fruitRouter } from "./api/fruitRoutes";
 const app = express();
 const port = 8080;
 
+// CORS
+app.use(function (req, res, next) {
+  res.header("Access-Control-Allow-Origin", "*");
+  res.header(
+    "Access-Control-Allow-Headers",
+    "Origin, X-Requested-With, Content-Type, Accept, Authorization"
+  );
+  res.header("Access-Control-Allow-Methods", "POST, GET, OPTIONS");
+  next();
+});
+
 // Parse JSON bodies for this app. Make sure you put
 // `app.use(express.json())` **before** your route handlers!
 app.use(express.json());
