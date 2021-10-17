a6588b69736b51bb930d8a97a17b7e91f5e925ba
Ben Ng <neong83@hotmail.com>
2021-10-16 18:41:02 -0700

    rename index file extension

--- backend/src/index.ts
+++ backend/src/index.ts
@@ -1,9 +1,12 @@
 import express from "express";
+import { router } from "./api/testRoutes";
 
 const app = express();
 const port = 8080;
 
-app.get("/", (req, res) => {
+app.use("/api/v1", router);
+
+app.get("/", (req, res, next) => {
   res.send("Hello world");
 });
 
